465720075e7db5e0d97f4e4c00aa685011a0504d
evalish <eldarvalishev@gmail.com>
2017-03-20 17:19:39 +0300

    Start files.

---
+++ common.js
@@ -0,0 +1,12 @@
+var str = 'Web-developer'
+    number = 2,
+    numberSecond = 2.1,
+	bool = true,
+	undef,
+    nuull = null,
+    obj = {
+		   prop: "yo",
+		   prop: function () {
+		   }
+    };
+alert(obj);
