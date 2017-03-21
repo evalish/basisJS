0ed1c24105e77cc4508798b9adec089e25a03e73
evalish <eldarvalishev@gmail.com>
2017-03-21 12:39:24 +0300

    2. Примитивные и объектные данные. Обертки

--- common.js
+++ common.js
@@ -1,12 +1,29 @@
-var str = 'Web-developer'
+var str = 'Halk Hogan',
     number = 2,
     numberSecond = 2.1,
 	bool = true,
 	undef,
-    nuull = null,
-    obj = {
-		   prop: "yo",
-		   prop: function () {
-		   }
-    };
-alert(obj);
+    nuull = null;
+var obj = {
+	    prop: "yo",
+	    fu: function () {
+
+	    }
+    },
+    regexp = /baba/g,
+    func = function(){},
+    arr = [1,2,3];
+    obj.prop = 'loftblog';
+    arr[2] = 24;
+
+   /* Обертки  */
+   var num = String(124);
+   num.prop = 'Строка в числе!!!';
+   var numObj = new String(124);
+   numObj.prop = 2352352;
+
+   //console.log( typeof numObj.toString() );
+   //console.log( num );
+    
+    console.log( numObj );
+    console.log( numObj.toString());
