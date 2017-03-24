694044b5cd5f954c949ab75bea913ae2b89a0e21
evalish <eldarvalishev@gmail.com>
2017-03-24 15:36:17 +0300

    Урок 5. Числа.

--- common.js
+++ common.js
@@ -1,2 +1,11 @@
-var transport = 'мопед';
-(transport !== 'ноги') ? alert ('скоро будем') : alert ('добираться долго');
+var number = 205.35;
+//console.log( number.toFixed(2) );
+//console.log( number.toExponential() );
+//console.log( number.toPrecision(2) );
+//console.log( number.toString() );
+//console.log ( Math.sqrt(81) );
+//console.log ( Math.pow(5,2) );
+//console.log ( Math.floor(number) );
+//console.log ( Math.ceil(number) );
+//console.log ( Math.round(number) );
+console.log ( Math.PI );
