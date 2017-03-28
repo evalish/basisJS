6ab80210a67394423d104483e211ec8332ad23b2
evalish <eldarvalishev@gmail.com>
2017-03-28 22:42:02 +0300

    Урок 7. Boolean, undefined, null.

--- common.js
+++ common.js
@@ -1,7 +1,25 @@
-var str = '"string" is a data type';
-    str2 = "\"string\" is a data type";
-    longStr = 'длинная \n \tстрока \bполучилась';
-    //console.log( longStr.length );
-    //console.log( 'hello' + ' world');
-    console.log ('hello'.concat(' world'));
-   
+var obj = {},
+    param,
+    arr = [1,2,3];
+//console.log(Boolean(NaN));
+//.log(Boolean(""));
+//console.log(Boolean(0));
+//console.log(Boolean(undefined));
+//console.log(Boolean(null));
+
+//console.log(Boolean("fdgdfg"));
+//console.log(Boolean(45344));
+//console.log(Boolean(obj));
+var bool = "str";
+if (bool) {
+	//console.log ( "Hi man!")
+};
+var func = function(arg) {
+	console.log(arg)
+
+};
+console.log( param );
+
+console.log( obj.paramp );
+console.log( arr[7] );
+func("Bill");
