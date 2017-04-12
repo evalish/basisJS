2df13a305033df9c8596347e75b7d53c26ea52dd
evalish <eldarvalishev@gmail.com>
2017-04-12 19:43:04 +0300

    Урок 15. Объект window и BOM.

--- common.js
+++ common.js
@@ -1,16 +1,18 @@
-var firstFunc = function () {
-	var index = 5;
+var foo = 'String';
 
-	return function() {
-		return index;
+//console.log( navigator.userAgent );
+// console.log( navigator.platform );
+// console.log( screen.width + ' x '+screen.height );
+// console.log( location.toString() );
+// console.log( frames );
+// console.log( history );
+// window.confirm( 'BOM' );
 
-	};
+var bar = window.prompt();
+
+if ( bar === "Привет") {
+	alert( "Привет и тебе от BOM!" )
+} else {
+	alert( "Пока!" );
 };
 
-var secondFunc = function(){
-	var index = 15;
-
-	console.log( firstFunc()() );
-};
-
-secondFunc();
