f4516222e71ec583d5276cb7094b5dd3ba10dcaa
evalish <eldarvalishev@gmail.com>
2017-04-13 18:02:42 +0300

    Урок 17. Создание элементов динамически.

--- common.js
+++ common.js
@@ -1,27 +1,20 @@
 (function() {
- var elems = document.getElementsByTagName("p"),
-     classElems = document.getElementsByClassName("paragraph"),
-     idElem = document.getElementById("four");
-     elemSelector = document.querySelector("p");
-     elemsSelector = document.querySelectorAll("p");
-     elemsInDiv = document.querySelectorAll("div p");
-  // console.log( elemsInDiv );
-  //console.log( elemsSelector ) ;  
- //console.log( elemsSelector );
- //console.log( idElem );
- //console.log( classElems );
- 
- //console.log( elems );
 
- for (var i = 0, len = elems.length; i < len; i++ ) {
- //console.log( elems[i].tagName );
- //console.log( elems[i].parentNode );
- //console.log( elems[i].previousSibling );
- //console.log( elems[i].previousSibling.previousSibling);
- //console.log( elems[i].previousSibling.nodeName);
-//console.log( elems[i].nodeType );
+var doc = document,
+    elem = doc.createElement("p"), //создать узел элемента
+    content = doc.createTextNode("Это текстовый узел"), //создать текстовый узел
+    wrappedP = doc.getElementById("wrapped");
+//elem.innerHTML = '<strong>Это динамически созданный элемент</strong>';
+//elem.appendChild(content);
+ //elem.setAttribute("id", "mrCook");
+//elem.id = "mrCook";
+//wrappedP.parentNode.removeChild(elem);
+//wrappedP.parentNode.removeChild(wrappedP);
+elem.appendChild(content);
 
- }
+console.log( elem );
 
+wrappedP.parentNode.appendChild(elem);
+//wrappedP.parentNode.insertBefore(elem, wrappedP);
+//wrappedP.parentNode.replaceChild(elem, wrappedP);
 })();
-console.log( document.querySelector("div").children );
